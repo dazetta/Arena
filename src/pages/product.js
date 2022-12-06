2b9c2ab19b86f18622180b681dc866eb07597b40
Abhimanyu Malik <abhimanyu513@indovations.in>
2022-12-07 04:35:29 +0530

    updates

--- src/pages/product.js
+++ src/pages/product.js
@@ -21,7 +21,7 @@ export default function Product() {
       </div>
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-          <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none">
             <img
               src={selectedProduct.Product_Detail_Image}
               alt={""}
