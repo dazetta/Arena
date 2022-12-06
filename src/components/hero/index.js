665f7dd2bfd27cfce44b9c9d8cd17daa85bf2d59
Abhimanyu Malik <abhimanyu513@indovations.in>
2022-12-06 15:01:55 +0530

    updates

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,22 +1,24 @@
+import heroSection from "../../data/hero";
+
 export default function Hero() {
   return (
-    <div className="bg-[url(https://i.postimg.cc/yddfKVmC/andrew-tanglao-3-I2vzcm-Ep-LU-unsplash.jpg)] bg-cover bg-bottom">
+    <div
+      className={`bg-[url(https://i.postimg.cc/yddfKVmC/andrew-tanglao-3-I2vzcm-Ep-LU-unsplash.jpg)] bg-cover bg-bottom`}
+    >
       <main>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
             <div>
               <div>
-                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                  DaZetta - Arena
+                <h1 className="text-4xl font-bold tracking-tight text-[#0351aa] sm:text-center sm:text-6xl">
+                  {heroSection?.Hero_Heading}
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat aliqua.
+                <p className="mt-6 text-lg leading-8 text-black sm:text-center">
+                  {heroSection?.Hero_Text}
                 </p>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                   <button className="inline-block rounded-full bg-[#0351aa] px-10 py-2 text-white shadow-sm">
-                    Get started
+                    {heroSection?.Hero_Button_Text}
                   </button>
                 </div>
               </div>
