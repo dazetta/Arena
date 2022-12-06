9deeb3e6ee834aa5b579a6684a8ce457292e51fe
Abhimanyu Malik <abhimanyu513@indovations.in>
2022-12-07 04:33:54 +0530

    updates

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,8 +1,80 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+
+import products from "../data/products";
+import categories from "../data/categories";
+import { convertToSlug } from "../utils";
 
 export default function Category() {
   let { slug } = useParams();
+  const navigate = useNavigate();
 
-  return <div>Product - {slug}</div>;
+  const productNavigate = (name) => {
+    return navigate(`/product/${convertToSlug(name)}`);
+  };
+
+  const selectedCategory = categories?.filter(
+    (e) => convertToSlug(e.Category_Name) === slug
+  )?.[0];
+
+  const categoryProducts = products.filter(
+    (e) => e.Category_Id === selectedCategory?.Category_Id
+  );
+
+  return (
+    <div className="bg-white">
+      <img
+        src={selectedCategory.Category_Detail_Image}
+        alt={""}
+        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+      />
+      <div className="bg-[#0351aa] py-5">
+        <h2 className="text-2xl text-center font-bold tracking-tight text-white">
+          {selectedCategory.Category_Name}
+        </h2>
+      </div>
+      <div className="mx-auto text-center max-w-2xl px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+          <p>{selectedCategory?.Category_Description}</p>
+      </div>
+      <div className="mx-auto max-w-2xl pb-16 px-4 sm:pb-12 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+          {categoryProducts.map((product, index) => (
+            <div key={index}>
+              <div
+                key={index}
+                className="group relative"
+                onClick={() => productNavigate(product.Product_Name)}
+              >
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={product.Product_Thumbnail_Image}
+                    alt={""}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm font-medium text-gray-900">
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.Product_Name}
+                    </h3>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    ${product.Product_Price}
+                  </p>
+                </div>
+              </div>
+              <h3
+                className="mt-4 text-center text-sm text-[#0351aa] font-semibold underline"
+                onClick={() => productNavigate(product.Product_Name)}
+              >
+                View
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/product.js
+++ src/pages/product.js
@@ -13,50 +13,48 @@ export default function Product() {
   )?.[0];
 
   return (
-    <div>
-      <div className="bg-white">
-        <div className="bg-[#0351aa] py-5">
-          <h2 className="text-2xl text-center font-bold tracking-tight text-white">
-            {selectedProduct.Product_Name}
-          </h2>
-        </div>
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-              <img
-                src={selectedProduct.Product_Detail_Image}
-                alt={""}
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
+    <div className="bg-white">
+      <div className="bg-[#0351aa] py-5">
+        <h2 className="text-2xl text-center font-bold tracking-tight text-white">
+          {selectedProduct.Product_Name}
+        </h2>
+      </div>
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
+          <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img
+              src={selectedProduct.Product_Detail_Image}
+              alt={""}
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div>
+            <h1 className="text-lg font-medium text-gray-900">
+              {selectedProduct.Product_Name}
+            </h1>
+            <p className="text-2xl font-medium text-gray-900">
+              ${selectedProduct.Product_Price}
+            </p>
+            <div className="mt-4 flex gap-1">
+              {[1, 2, 3, 4, 5].map((e, i) => (
+                <StarIcon className="text-[#0351aa] w-5 h-5" />
+              ))}
             </div>
-            <div>
-              <h1 className="text-lg font-medium text-gray-900">
-                {selectedProduct.Product_Name}
-              </h1>
-              <p className="text-2xl font-medium text-gray-900">
-                ${selectedProduct.Product_Price}
-              </p>
-              <div className="mt-4 flex gap-1">
-                {[1, 2, 3, 4, 5].map((e, i) => (
-                  <StarIcon className="text-[#0351aa] w-5 h-5" />
-                ))}
-              </div>
-              <div className="mt-5">
-                <h4 className="text-sm text-gray-900 font-bold">Size:</h4>
-                <select className="py-2 px-4 border mt-2 border-gray-900">
-                  <option>12</option>
-                  <option>13</option>
-                  <option>14</option>
-                  <option>15</option>
-                </select>
-              </div>
-              <div className="mt-4">{selectedProduct.Product_Description}</div>
-              <div className="mt-4">
-                <button className="flex items-center gap-3 rounded-full bg-[#0351aa] px-10 py-2 text-white shadow-sm">
-                  Add to Cart
-                  <ShoppingCartIcon className="w-5 h-5" />
-                </button>
-              </div>
+            <div className="mt-5">
+              <h4 className="text-sm text-gray-900 font-bold">Size:</h4>
+              <select className="py-2 px-4 border mt-2 border-gray-900">
+                <option>12</option>
+                <option>13</option>
+                <option>14</option>
+                <option>15</option>
+              </select>
+            </div>
+            <div className="mt-4">{selectedProduct.Product_Description}</div>
+            <div className="mt-4">
+              <button className="flex items-center gap-3 rounded-full bg-[#0351aa] px-10 py-2 text-white shadow-sm">
+                Add to Cart
+                <ShoppingCartIcon className="w-5 h-5" />
+              </button>
             </div>
           </div>
         </div>
