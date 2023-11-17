3550f314455560689532aa6868d7e5c788c6a547
gaurav-dazetta <gaurav.dasila@dazetta.co>
2023-11-17 18:33:38 +0530

    Updated

--- src/data/hero.js
+++ src/data/hero.js
@@ -3,7 +3,7 @@ const heroSection = {
     "https://dazetta.co/wp-content/uploads/2023/05/hero-banner.jpg",
   Hero_Heading: "DaZetta - Arena",
   Hero_Text:
-    "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
+    "Stride in Style: Elevate Your Run with Our Cutting-Edge Running Gear!",
   Hero_Button_Text: "Get started",
   Hero_Button_Link: "#",
 };
