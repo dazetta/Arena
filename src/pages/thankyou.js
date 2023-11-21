478ab51cc62024d62ff80dcec92b48a6e5aad283
Rajneesh Gautam <rajneesh@pierian.co>
2023-11-21 19:06:39 +0000

    Added product quantity

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -21,7 +21,8 @@ export default function Thankyou() {
       "product_id": order?.products.map(order => order.Product_Id),
       "product_name": order?.products.map(order => order.Product_Name),
       "product_price": order?.products.map(order => order.Product_Price),
-      "product_category": order?.products.map(order => categories.filter((category) => order.Category_Id == order?.Category_Id)[0].Category_Name) 
+      "product_quantity": order?.products.map(function(){return 1}),
+      "product_category": order?.products.map(order => categories.filter((category) => category.Category_Id == order?.Category_Id)[0].Category_Name) 
     }
     auth.user_id && (dataLayer["customer_id"] = auth.user_id);
     order?.user_email && (dataLayer["customer_id"] = order?.user_email);
