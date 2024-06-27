a5b9293d6050fb5c22a7049903df5ed985d0bcbf
gaurav-dazetta <gaurav.dasila@dazetta.co>
2024-06-27 14:26:19 +0530

    Update index.js

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -6,8 +6,8 @@ export default function Hero() {
   return (
     <div>
       <main>
-        <div className="relative px-6 lg:px-8 hero-section font-montserrat">
-          <img src={heroImage} className="absolute left-0 top-0 z-[9] w-full h-full object-cover" />
+        <div className="relative px-6 lg:px-8 font-montserrat">
+          <img src={heroImage} className="absolute left-0 top-0 z-[9] w-full h-full object-cover object-left" />
           <div className="mx-auto max-w-7xl pt-20 pb-32 sm:pt-48 sm:pb-40 z-[99] relative">
             <h1 className="text-4xl font-bold tracking-tight text-secondary sm:text-6xl text-left">
               {heroSection?.Hero_Heading}
