d964694f459924b48c6a1ee927ac0fe50a74943d
sirishdexata <141637553+sirishdexata@users.noreply.github.com>
2024-06-28 18:52:16 +0530

    Search Module

---
+++ src/data/dummyData.js
@@ -0,0 +1,44 @@
+const searchData = {
+    "shoes": [
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/arena-shoes-1.png",
+            "productTitle": "Arena AgileGlide Mens Shoes",
+            "productDesp": "Glide effortlessly through your runs with Arena AgileGlide. Engineered for agility, these shoes offer lightweight comfort and superior traction. Stay swift and focused on your goals with every stride.",
+            "productLink": "/product/arena-agileglide-mens-shoes"
+        },
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/arena-shoes-2.png",
+            "productTitle": "Arena VelocityVibe Mens Shoes",
+            "productDesp": "Experience the rush of speed with Arena VelocityVibe. Designed for runners seeking maximum velocity, these shoes provide exceptional responsiveness and energy return. Propel forward with confidence and style.",
+            "productLink": "/product/arena-velocityvibe-mens-shoes"
+        },
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/arena-shoes-6.png",
+            "productTitle": "Arena EnduraStride Womens Shoes",
+            "productDesp": "Endure through any challenge with Arena EnduraStride. These women's shoes are built for durability and comfort, providing reliable support mile after mile. Conquer your goals with confidence and resilience.",
+            "productLink": "/product/arena-endurastride-womens-shoes"
+        }
+    ],
+    "electronics": [
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/7.png",
+            "productTitle": "Arena Sprinter Pro Chrono Watch",
+            "productDesp": "Engineered for athletes, this watch offers precise timing, durable construction, and essential features for tracking performance. Conquer your goals with confidence and style.",
+            "productLink": "/product/arena-sprinter-pro-chrono-watch"
+        },
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/1-2.png",
+            "productTitle": "Arena PowerBeat Pro Sport Buds",
+            "productDesp": "Elevate your workout experience with these earphones, delivering powerful sound and a secure fit. Designed for athletes, they offer long-lasting comfort and reliable performance to keep you motivated.",
+            "productLink": "/product/arena-powerbeat-pro-sport-buds"
+        },
+        {
+            "productImageLink": "https://dexata.co/wp-content/uploads/2024/02/2-1.png",
+            "productTitle": "Arena PulseWave Premium Sports Headphones",
+            "productDesp": "Feel the rhythm of your workout with these premium headphones, delivering superior sound quality and ergonomic design. With advanced features and long-lasting comfort, they keep you in the zone for peak performance.",
+            "productLink": "/product/arena-pulsewave-premium-sports-headphones"
+        }
+    ]
+}
+
+export default searchData;
---
+++ src/pages/searchResult/searchResult.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import "./searchResult.css";
+import { useLocation, useNavigate, Link } from 'react-router-dom';
+import searchData from '../../data/dummyData';
+import Promotion from "../../components/searchpromo";
+import SecondaryButton from '../../components/Buttons/SecondaryButton';
+
+const Search = () => {
+    const navigate = useNavigate();
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const query = queryParams.get('query');
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        if (searchData[query]) {
+            setFilteredProducts(searchData[query]);
+        } else {
+            setFilteredProducts([]);
+        }
+        setSearchQuery(""); // Clear the search box when query changes
+    }, [query]);
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        if (searchQuery.length > 0) {
+            if (searchData[searchQuery]) {
+                setFilteredProducts(searchData[searchQuery]);
+            } else {
+                setFilteredProducts([]);
+            }
+            navigate(`/search?query=${searchQuery}`);
+        }
+    }
+
+    return (
+        <div className="space-y-8">
+            <div className='relative'>
+                <img
+                    src="https://dexata.co/wp-content/uploads/2024/05/search-banner-image.png"
+                    alt=""
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+                <div className='absolute top-0 left-0 right-0 m-auto z-[99] mx-auto max-w-2xl pb-16 px-4 sm:pb-12 sm:px-6 lg:max-w-7xl lg:px-8 font-montserrat'>
+                    <h2 className="font-montserrat leading-normal text-center text-secondary text-4xl font-bold mb-4 mt-8">Search results for: " {query} "</h2>
+                    <form onSubmit={handleSubmitForm} className="relative search-form">
+                        <input
+                            autoFocus
+                            type="text"
+                            value={searchQuery}
+                            onChange={(e) => {
+                                setSearchQuery((e.target.value).toLowerCase());
+                            }}
+                            className="search-input bg-gray-50 border border-gray-10 text-gray-900 sm:text-sm rounded-full p-2.5 pl-4 pr-10 focus:outline-none"
+                            placeholder="Search Keyword"
+                        />
+                        <button type="submit" className="search-btn">
+                            <img className="search-image" src="https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-search-icon-png-image_4184112.jpg" alt="search-logo" />
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div className="mx-auto max-w-2xl pb-16 px-4 sm:pb-12 sm:px-6 lg:max-w-7xl lg:px-8 font-montserrat">
+                <Promotion />
+                <div className="mt-6">
+                    {filteredProducts?.length > 0 ? (
+                        filteredProducts.map((product, index) => (
+                            <div className="mt-6 flex flex-col items-center bg-white border border-gray p-4 w-full rounded-lg shadow md:flex-row" key={index}>
+                                <Link to={product.productLink} className='flex items-start'>
+                                    <img className="rounded-lg w-64 h-[200px] object-cover" src={product.productImageLink} alt="product thumbnail" />
+                                    <div className="ml-4">
+                                        <h5 className="mb-4 text-lg font-semibold tracking-tight text-black">{product.productTitle}</h5>
+                                        <p className="text-left mb-4">
+                                            {product.productDesp}
+                                        </p>
+                                        <SecondaryButton onClick={() => {
+                                            navigate(product.productLink);
+                                        }}>
+                                            Buy Now
+                                        </SecondaryButton>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center w-full">
+                            <h1 className="text-3xl text-gray-900 font-semibold text-center">No search results found</h1>
+                            <img width="270" src="https://www.avisa.care//assets/img/No_Product_Found.png" alt="no product found" />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
